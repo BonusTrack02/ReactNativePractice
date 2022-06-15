db9c61344a1377135d6bd027b9e2fd333b4431d7
BonusTrack02 <wlsgud321@gmail.com>
2022-06-15 15:48:22 +0900

    중첩 구조 배치 연습

--- Ex05Layout/Main.js
+++ Ex05Layout/Main.js
@@ -44,10 +44,54 @@ export default class Main extends Component {
 
             //수평 배치일 때
             //justifyContent, alignItems 정렬 속성 연습
-            <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between", alignItems:"center"}}>
-                <View style={{ width: 50, height: 50, backgroundColor: "red" }}></View>
-                <View style={{ width: 50, height: 50, backgroundColor: "green", alignSelf:"flex-start" }}></View>
-                <View style={{ width: 50, height: 50, backgroundColor: "blue" }}></View>
+            // <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between", alignItems:"center"}}>
+            //     <View style={{ width: 50, height: 50, backgroundColor: "red" }}></View>
+            //     <View style={{ width: 50, height: 50, backgroundColor: "green", alignSelf:"flex-start" }}></View>
+            //     <View style={{ width: 50, height: 50, backgroundColor: "blue" }}></View>
+            // </View>
+
+            // <View style={{flex:1}}>
+            //     <View style={{height:50, backgroundColor:"red"}}></View>
+
+            //     <View style={{flex:1, backgroundColor:"green"}}></View>
+            //     <View style={{flex:1, backgroundColor:"blue"}}></View>
+            // </View>
+
+            //중첩 구조의 배치 - 수직, 수평 배치 혼재
+            <View style={{flex:1, flexDirection:"column"}}>
+                {/* 크게 수직으로 2분할 [1:2비율] */}
+                <View style={{flex:1, flexDirection:"row"}}>
+                    {/* 좌우로 2:1의 비율 배치 */}
+                    <View style={{flex:2, backgroundColor:"red"}}>
+                        {/* 절대 위치를 이용하여 뷰들을 겹치도록 배치 */}
+                        <View style={{width:50, height:50, backgroundColor:"white", left:10, top:10, position:"absolute"}}></View>
+                        <View style={{width:50, height:50, backgroundColor:"gray", left:20, top:20, position:"absolute"}}></View>
+                    </View>
+                    <View style={{flex:1,}}>
+                        {/* 수직으로 1:1비율 배치 */}
+                        <View style={{flex:1, backgroundColor:'yellow'}}></View>
+                        <View style={{flex:1, backgroundColor:"green"}}></View>
+                    </View>
+                </View>
+
+                <View style={{flex:2, flexDirection:"row"}}>
+                    {/* 좌우로 1:2 비율 배치 */}
+                    <View style={{flex:1, backgroundColor:"blue"}}></View>
+                    <View style={{flex:2}}>
+                        {/* 수직 1:1 */}
+                        <View style={{flex:1, backgroundColor:"gray"}}></View>
+                        <View style={{ flex: 1, backgroundColor: "brown" }}>
+                            {/* 절대 위치를 이용하여 뷰들을 겹치도록 배치 */}
+                            <View style={{ width: 50, height: 50, backgroundColor: "white", left: 10, top: 10, position: "absolute" }}></View>
+                            <View style={{ width: 50, height: 50, backgroundColor: "gray", left: 20, top: 20, position: "absolute" }}></View>
+
+                            <View style={{ width: 50, height: 50, backgroundColor: "black", right: 20, bottom: 20, position: "absolute" }}></View>
+                        </View>
+                    </View>
+                </View>
+
+                {/* 절대 위치 */}
+                <View style={{width:100, height:100, backgroundColor:"orange", position:'absolute', bottom:50, left:80, borderRadius:50}}></View>
             </View>
         )
     }
