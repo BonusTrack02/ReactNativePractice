37c9f26ebe67e6a61dad492743774989b56a4584
BonusTrack02 <wlsgud321@gmail.com>
2022-06-15 13:56:03 +0900

    배경 이미지 적용

--- Ex04Image/MainComponent.js
+++ Ex04Image/MainComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {View, Image, TouchableOpacity, Alert, TouchableHighlight, TouchableNativeFeedback, Text} from 'react-native'
+import {View, Image, TouchableOpacity, Alert, TouchableHighlight, TouchableNativeFeedback, Text, ImageBackground} from 'react-native'
 
 class MainComponent extends Component {
 
@@ -19,55 +19,61 @@ class MainComponent extends Component {
 
     render() {
         return (
-            //전체 뷰에 배경 이미지 적용
+            //전체 뷰에 배경 이미지 적용 - 스타일로 적용 불가
+            //배경 이미지용 뷰가 따로 있다
             <View style={{flex:1}}>
-                {/* 그림 이미지는 require()함수 이용 - 별도의 스타일이 없다면 기본 원본 사이즈 */}
-                <Image
-                    style={{width:200, height:100}}
-                    source={require("./image/moana01.jpg")}>
-                </Image>
-
-                {/* 네트워크 상의 이미지를 보여주기 : uri 라는 이름의 멤버를 가진 객체를 source로 설정 */}
-                {/* 네트워크 상의 이미지는 사이즈 지정이 없으면 보이지 않는다 */}
-                <Image
-                    style={{width:200, height:100}}
-                    source={{uri:'https://cdn.pixabay.com/photo/2018/03/26/02/05/cat-3261420_960_720.jpg'}}>
-                </Image>
-
-                {/* 이미지의 클릭 이벤트 처리 - Image 컴포넌트는 onPress라는 속성이 없다 */}
-                {/* 클릭이벤트에 반응하는 컴포넌트들이 별도로 존재한다 - TouchableXXXXXXX */}
-                <TouchableOpacity onPress={this.clickImage}>
+                <ImageBackground
+                    source={{uri:"https://cdn.pixabay.com/photo/2022/05/31/14/17/sea-7233669_960_720.jpg"}}
+                    style={{width:"100%",height:"100%",}}
+                    resizeMode="stretch">
+                    {/* 그림 이미지는 require()함수 이용 - 별도의 스타일이 없다면 기본 원본 사이즈 */}
                     <Image
                         style={{ width: 200, height: 100 }}
-                        source={require('./image/moana02.jpg')}>
+                        source={require("./image/moana01.jpg")}>
                     </Image>
-                </TouchableOpacity>
 
-                <TouchableHighlight onPress={this.clickImage} style={{padding:4,width:208}}>
+                    {/* 네트워크 상의 이미지를 보여주기 : uri 라는 이름의 멤버를 가진 객체를 source로 설정 */}
+                    {/* 네트워크 상의 이미지는 사이즈 지정이 없으면 보이지 않는다 */}
                     <Image
                         style={{ width: 200, height: 100 }}
-                        source={require('./image/moana03.jpg')}>
+                        source={{ uri: 'https://cdn.pixabay.com/photo/2018/03/26/02/05/cat-3261420_960_720.jpg' }}>
                     </Image>
-                </TouchableHighlight>
 
-                {/* 안드로이드의 클릭 피드백 효과(물결효과) */}
-                <TouchableNativeFeedback
-                    onPress={this.clickImage}>
-                    <View style={{ padding: 4, width: 208, borderWidth: 2, borderRadius: 4, marginLeft: 16 }}>
-                        <Text>MOANA</Text>
+                    {/* 이미지의 클릭 이벤트 처리 - Image 컴포넌트는 onPress라는 속성이 없다 */}
+                    {/* 클릭이벤트에 반응하는 컴포넌트들이 별도로 존재한다 - TouchableXXXXXXX */}
+                    <TouchableOpacity onPress={this.clickImage}>
                         <Image
                             style={{ width: 200, height: 100 }}
-                            source={require('./image/moana04.jpg')}>
+                            source={require('./image/moana02.jpg')}>
                         </Image>
-                    </View>
-                </TouchableNativeFeedback>
+                    </TouchableOpacity>
 
-                <TouchableOpacity onPress={this.changeImage}>
-                    <Image
-                        style={{ width: 200, height: 100 }}
-                        source={this.state.imgArr[this.state.imgNum]}>
-                    </Image>
-                </TouchableOpacity>
+                    <TouchableHighlight onPress={this.clickImage} style={{ padding: 4, width: 208 }}>
+                        <Image
+                            style={{ width: 200, height: 100 }}
+                            source={require('./image/moana03.jpg')}>
+                        </Image>
+                    </TouchableHighlight>
+
+                    {/* 안드로이드의 클릭 피드백 효과(물결효과) */}
+                    <TouchableNativeFeedback
+                        onPress={this.clickImage}>
+                        <View style={{ padding: 4, width: 208, borderWidth: 2, borderRadius: 4, marginLeft: 16 }}>
+                            <Text>MOANA</Text>
+                            <Image
+                                style={{ width: 200, height: 100 }}
+                                source={require('./image/moana04.jpg')}>
+                            </Image>
+                        </View>
+                    </TouchableNativeFeedback>
+
+                    <TouchableOpacity onPress={this.changeImage}>
+                        <Image
+                            style={{ width: 200, height: 100 }}
+                            source={this.state.imgArr[this.state.imgNum]}>
+                        </Image>
+                    </TouchableOpacity>
+                </ImageBackground>
             </View>
         )
     }
