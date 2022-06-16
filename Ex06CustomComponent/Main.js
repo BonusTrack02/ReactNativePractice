4f1bb5c033e5e96ac8c846b8824b26ac0329e207
BonusTrack02 <wlsgud321@gmail.com>
2022-06-16 11:43:13 +0900

    커스텀 컴포넌트 만들어 사용해보기

--- Ex06CustomComponent/Main.js
+++ Ex06CustomComponent/Main.js
@@ -5,8 +5,29 @@ export default class Main extends Component {
     render() {
         return (
             <View>
-                <Text>Hello world</Text>
+                <Text style={style.text}>Hello world</Text>
+
+                {/* 개발자가 만든 Custom Component를 사용해보기 */}
+                <MyComponent></MyComponent>
+                <MyComponent></MyComponent>
             </View>
         )
     }
 }
+
+//사용자 정의 컴포넌트 클래스
+class MyComponent extends Component {
+    render() {
+        return (
+            <View style={{margin:8}}>
+                <Text>Hello sam</Text>
+                <Button title="click me"></Button>
+            </View>
+        )
+    }
+}
+
+const style = StyleSheet.create({
+    root:{flex:1},
+    text:{color:"black"},
+})
