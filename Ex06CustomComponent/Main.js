23fe0a956ba6463048407872b7dd2903ec0c30e3
BonusTrack02 <wlsgud321@gmail.com>
2022-06-16 11:34:08 +0900

    6번 예제 생성

---
+++ Ex06CustomComponent/Main.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {View, Text, StyleSheet, Button} from 'react-native'
+
+export default class Main extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello world</Text>
+            </View>
+        )
+    }
+}
