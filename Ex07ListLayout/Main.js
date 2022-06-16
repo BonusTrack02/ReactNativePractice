9c63aeb6289c694d53dadfc83f583e674cfd8707
BonusTrack02 <wlsgud321@gmail.com>
2022-06-16 14:45:07 +0900

    메소드로 컴포넌트 리턴하기

--- Ex07ListLayout/Main.js
+++ Ex07ListLayout/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default class Main extends Component {
     render() {
@@ -9,9 +9,32 @@ export default class Main extends Component {
         //JSX 컴포넌트 객체를 변수에 저장
         const aaa = <Text>Nice</Text>
 
+        //변수에 컴포넌트 그룹도 저장 가능
+        const bbb = <View style={{margin:8}}>
+            <Text>Hello RN</Text>
+            <Button title="button"></Button>
+        </View>
+
         return (
             <View style={style.root}>
                 <Text style={style.text}>List layout</Text>
+                {aaa}
+
+                {bbb}
+                {bbb}
+
+                {/* 컴포넌트를 리턴하는 함수를 호출 */}
+                {this.showItemView()}
+            </View>
+        )
+    }
+
+    //컴포넌트를 리턴하는 메소드 정의
+    showItemView (){
+        return (
+            <View style={{margin:8}}>
+                <Text>Nice world</Text>
+                <Button title="press me"></Button>
             </View>
         )
     }
